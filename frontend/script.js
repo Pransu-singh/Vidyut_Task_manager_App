e39b20b41d64eaf71822e8896372d5f0a1de22d4
Pransu Singh <160752422+Pransu-singh@users.noreply.github.com>
2026-03-20 16:39:35 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 //  Configuration
 // ─────────────────────────────────────────
 
-const API_BASE = "http://127.0.0.1:5000";  // Flask backend URL
+const API_BASE = import.meta.env.VITE_API_URL;  // Flask backend URL
 
 // ─────────────────────────────────────────
 //  State
